6922742601a2bbcc4e9f7fa6af04220a53d509ca
polowis <tranphuochung0912@gmail.com>
2020-03-05 18:58:10 +1100

    Update __init__.py

--- app/http/controllers/__init__.py
+++ app/http/controllers/__init__.py
@@ -8,13 +8,17 @@ controller_files = os.listdir('./app/http/controllers')
 controllers = {}
 files = []
 
+def file_endswith(filename, suffix):
+    """Check if file correctly ends"""
+    return filename.endswith(suffix) or filename.endswith(suffix.lower())
+
 for controller_file in controller_files:
     filename, file_extension = os.path.splitext(controller_file)
     if file_extension == '.py':
         controllers.update({filename: file_extension})
 
 for controller_name in controllers.keys():
-    if controller_name.endswith('Controller') or controller_name.endswith('controller'):
+    if file_endswith(controller_name, 'Controller'):
         files.append(controller_name)
 
 
