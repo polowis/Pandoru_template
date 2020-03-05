651a284251d2a5339f8868c43dc48eb592ed7dee
polowis <tranphuochung0912@gmail.com>
2020-03-05 18:43:24 +1100

    Update __init__.py

--- app/http/controllers/__init__.py
+++ app/http/controllers/__init__.py
@@ -1,9 +1,10 @@
-"""from app.http.controllers import *"""
+
 
 import os
 controller_files = os.listdir('./app/http/controllers')
 controllers = {}
 files = []
+
 for controller_file in controller_files:
     filename, file_extension = os.path.splitext(controller_file)
     if file_extension == '.py':
