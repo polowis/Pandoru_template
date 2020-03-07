66c9b28c1f520793c679457631ced258307ba06b
polowis <tranphuochung0912@gmail.com>
2020-03-07 13:11:09 +1100

    added csrf view

--- app/http/exception/view_exception.py
+++ app/http/exception/view_exception.py
@@ -5,15 +5,18 @@ from flask_wtf.csrf import CSRFError
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """Handle 404 error"""
     error = { 'title': '404 | Page not found', 'message': 'Sorry! Page not found'}
     return render_template('error/404.html', error=error)
 
 @app.errorhandler(500)
 def page_not_found(e):
+    """Handle 500 error"""
     error = { 'title': '500 | Internal server error', 'message': 'Internal server error'}
     return render_template('error/500.html', error=error)
 
 @app.errorhandler(CSRFError)
 def page_csrf(e):
+    """Handle csrf error"""
     error = { 'title': '419 | Page expired', 'message': 'Page expired'}
     return render_template('error/419.html', error=error)
