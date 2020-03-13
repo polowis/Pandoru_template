5e06094bc9d14fc624c689282f2a99a1eeb9d448
polowis <tranphuochung0912@gmail.com>
2020-03-13 13:58:48 +1100

    add user model

---
+++ app/model/user.py
@@ -0,0 +1,10 @@
+from app import db
+from sqlalchemy.sql import func
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
