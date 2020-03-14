a7d9ca6f5a63cef1c077dd43d7a24dc3078b3c12
polowis <tranphuochung0912@gmail.com>
2020-03-14 12:06:59 +1100

    build: add config example file

--- app/config.py
+++ app/config.py
@@ -1,6 +1,27 @@
 import os
 class Config(object):
+    # enable debug
     DEBUG_ENABLED = False
+
+    #enable testing
     TESTING = False
+
+    #Enable CSRF protection
     CSRF_ENABLED = True
+
+    #App's Key. 
     SECRET_KEY = "50243a674f1d241ee02217580ed14eb524828c95f80e2acc59bd56d6025277ee"
+
+    #Database property
+    DB_CONNECTION='mysql'
+    DB_HOST='127.0.0.1'
+    DB_PORT='3306'
+    DB_DATABASE='flask1'
+    DB_USERNAME='root'
+    DB_PASSWORD=''
+
+    #DB_URI is optional
+    DB_URI = f'{DB_CONNECTION}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
+    
+    DATABASE_CONNECT_OPTIONS = {}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
