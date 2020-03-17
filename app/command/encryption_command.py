02b2abef6ae2d31e7563b9331abe31c882efbe9f
polowis <tranphuochung0912@gmail.com>
2020-03-18 10:04:54 +1100

    ci : add encrytion command

---
+++ app/command/encryption_command.py
@@ -0,0 +1,11 @@
+from app import app
+import bcrypt
+from app.framework.util.color import *
+import click
+
+@app.cli.command('hash')
+@click.argument('password')
+def hash(password):
+    hash_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    info('The encrypted string is:')
+    warn(str(hash_password))
