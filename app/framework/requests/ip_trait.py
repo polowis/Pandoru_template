84dde8a5bbff359556e01f1dc441ae878d2338e4
polowis <tranphuochung0912@gmail.com>
2020-05-07 13:18:28 +1000

    feat: add ip detection util

---
+++ app/framework/requests/ip_trait.py
@@ -0,0 +1,83 @@
+import socket
+import requests
+
+def is_valid_ipv4_address(address):
+    """Return true if valid IPv4 address"""
+    try:
+        if(isinstance(address, str) == False):
+            raise socket.error
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            if(isinstance(address, str) == False):
+                raise socket.error
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  # not a valid address
+        return False
+
+    return True
+
+def is_valid_ipv6_address(address):
+    """return true if valid ipv6 address"""
+    try:
+        if(isinstance(address, str) == False):
+            raise socket.error
+        socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:  # not a valid address
+        return False
+    return True
+
+def is_valid_ip(address):
+    return is_valid_ipv4_address(address) or is_valid_ipv6_address(address)
+
+
+class IP:
+    """Class base to handle incoming user ip address"""
+    def __init__(self, ip):
+        self.ip = ip
+        self.details = self.__getDetails()
+        self.address = self.get_address_name_by_ip()
+        self.city = self.get_city_name_by_ip()
+        self.state = self.get_state_name_by_ip()
+        self.region = self.get_region_name_by_ip()
+        self.country = self.get_country_name_by_ip()
+        self.country_code = self.details["geoplugin_countryCode"]
+    
+    def get_country_name_by_ip(self):
+        """return country name"""
+        return self.details["geoplugin_countryName"]
+
+    def get_city_name_by_ip(self):
+        """return city name"""
+        return self.details["geoplugin_city"]
+
+    def get_state_name_by_ip(self):
+        """return state name"""
+        return self.details["geoplugin_region"]
+    
+    def get_region_name_by_ip(self):
+        """return region name"""
+        return self.details["geoplugin_region"]
+    
+    def get_address_name_by_ip(self):
+        """Return countryName, regionName and cityName"""
+        address = []
+        address.append(self.country)
+
+        if(len(self.state) >= 1):
+            address.append(self.state)
+
+        if(len(self.city) >=1):
+            address.append(self.city)
+        return ', '.join(address.reverse()) 
+        
+
+    def __getDetails(self):
+        """Return all you need to know about this ip address"""
+        url = 'http://www.geoplugin.net/json.gp?ip={}'.format(self.ip)
+        res = requests.get(url)
+        j = json.loads(res.text)
+        return j
--- app/framework/requests/request.py
+++ app/framework/requests/request.py
@@ -1,7 +1,13 @@
 from flask import request as req
+from ..util import *
+from .ip_trait import *
+import requests, json
 
 class Request:
     """Handle request, response"""
+    def IP(self):
+        return IP(self.ip())
+
     def input(self, name=None):
         """Get input form data"""
         if name is None:
@@ -67,6 +73,50 @@ class Request:
     def get_json(self):
         """Return json"""
         return req.get_json()
+    
+    def header(self, value=None):
+        """Get header request"""
+        if value is None:
+            return req.headers
+        return req.headers.get(value)
 
+    def platform(self, **kwargs):
+        """Return request's OS platform"""
+        return req.user_agent.platform
+    
+    def browser(self, **kwargs):
+        """Return request's browser"""
+        return req.user_agent.browser
+    
+    def version(self, **kwargs):
+        """Return browser version"""
+        return req.user_agent.version
+
+    def ip(self, deep_detect=True):
+        """Return request's IP address
+        If you want to get more details, use method chaining IP instead
+        Eg: request.IP().state
+        """
+
+        ip = req.remote_addr
+        if deep_detect:
+            if(is_valid_ip(self.header("X-Forwarded-For"))):
+                ip = self.header("X-Forwarded-For")
+
+            if(is_valid_ip(self.header("X-Client-IP"))):
+                ip = self.header("X-Client-IP")
+
+        return ip
+
+    def location(self, **kwargs):
+        """Return request's location (country name)
+        If you want to get more details, use method chaining IP instead
+        Eg: request.IP().state
+        """
+        url = 'http://www.geoplugin.net/json.gp?ip={}'.format(self.ip())
+        r = requests.get(url)
+        j = json.loads(r.text)
+        locale = j["geoplugin_countryName"]
+        return locale
 
 request = Request()
