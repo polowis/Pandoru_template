d759a397ee07aceab1872c7ff253ef45b4a13df0
polowis <tranphuochung0912@gmail.com>
2020-09-07 16:42:52 +1000

    feat: display message upon success

--- app/command/configuration_command.py
+++ app/command/configuration_command.py
@@ -1,10 +1,14 @@
 from app import app
 import os
+from app.framework.util.color import *
 
 @app.cli.command('config:localization')
 def localization():
     os.system("sh app/framework/bin/copy_localization.sh")
+    info("Successfully generated localization config file")
+    
 
 @app.cli.command('config:mail')
 def mail():
     os.system("sh app/framework/bin/copy_mail.sh")
+    info("Successfully generated mailing config file")
