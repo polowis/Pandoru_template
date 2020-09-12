e99b24d9f8fb8518ea7ba6194070f886a7ad4fe7
polowis <tranphuochung0912@gmail.com>
2020-09-12 19:07:37 +1000

    fix: remove comments

--- bin/python.sh
+++ bin/python.sh
@@ -1,8 +1,8 @@
-# Set minimum required versions
+
 PYTHON_MINIMUM_MAJOR=3
 PYTHON_MINIMUM_MINOR=6
 
-# Get python references
+
 PYTHON3_REF=$(which python3 | grep "/python3")
 PYTHON_REF=$(which python | grep "/python")
 
@@ -15,7 +15,6 @@ python_ref(){
     echo $($my_ref -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(major); print(minor);')
 }
 
-# Print success_msg/error_msg according to the provided minimum required versions
 check_version(){
     local major=$1
     local minor=$2
@@ -23,7 +22,7 @@ check_version(){
     [[ $major -ge $PYTHON_MINIMUM_MAJOR && $minor -ge $PYTHON_MINIMUM_MINOR ]] && echo $python_ref || error_msg
 }
 
-# Logic
+
 if [[ ! -z $PYTHON3_REF ]]; then
     version=($(python_ref python3))
     check_version ${version[0]} ${version[1]} $PYTHON3_REF
