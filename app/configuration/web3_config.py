dc4c9718b385e7ff796fb19b57215b738d887871
polowis <tranphuochung0912@gmail.com>
2020-09-15 11:29:05 +1000

    feat: include dapp

---
+++ app/configuration/web3_config.py
@@ -0,0 +1,8 @@
+PORT=888
+PUBLIC_PORT=9999
+PROJECT_ID="" #<YOUR_INFURA_PROJECT_ID>
+ADDRESS="" #<YOUR_ACCOUNT>
+PRIVATE_KEY="" #<PRIVATE_KEY>
+IPC_PROVIDER=None #example: ./path/to/geth.ipc
+HTTP_PROVIDER="https://ropsten.infura.io/v3/" #example: http://127.0.0.1:8545 make sure to specify the port number
+WS_PROVIDER=None #example: ws://127.0.0.1:8546 
---
+++ app/http/controllers/TransactionController.py
@@ -0,0 +1,43 @@
+from web3 import Web3
+from app.framework.util import *
+from flask import jsonify
+import json
+from app import app
+from app.framework.controller import *
+from app.framework.requests.request import request
+from app.configuration.web3_config import *
+
+
+def get_provider():
+    return IPC_PROVIDER or HTTP_PROVIDER or WS_PROVIDER
+
+infura_url = HTTP_PROVIDER + PROJECT_ID
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+address_server=ADDRESS
+balance = web3.fromWei(web3.eth.getBalance(address_server), 'ether')
+
+class TransactionController(Controller):
+    def construct(cls):
+        TransactionController.register(app)
+
+    
+    @route('/api/ethereum/send')
+    def send_ethereum_transaction(self):
+        private_key = PRIVATE_KEY
+        nonce = web3.eth.getTransactionCount(address_server)
+
+        address_destination = data['address_destination']
+        value = data['value']
+        tx = {
+            'nonce': nonce,
+            'to': address_destination,
+            'value': web3.toWei(value, 'ether'),
+            'gas': 2000000,
+            'gasPrice': web3.toWei('50', 'gwei'),
+        }
+        signed_tx = web3.eth.account.signTransaction(tx, private_key)
+        tx_hash = web3.toHex(web3.eth.sendRawTransaction(signed_tx.rawTransaction))
+
+        balance_server = web3.fromWei(web3.eth.getBalance(address_server), 'ether')
+        return jsonify(message="Success")
