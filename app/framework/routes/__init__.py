e19ef2f810c5949b546f6c0e46362d23be3b8aba
polowis <tranphuochung0912@gmail.com>
2020-09-20 10:16:50 +1000

    fix: route should only accept class instead of parsing string

--- app/framework/routes/__init__.py
+++ app/framework/routes/__init__.py
@@ -2,6 +2,7 @@ from app import app
 from importlib import import_module
 from app.framework.routes.route_action import getFunction, getModule
 from flask import request
+from ..controller import Controller
 
 def next():
     """Redirect user to the next page"""
@@ -12,16 +13,12 @@ def back():
     return request.referrer
 
 class Route:
-    def get(self, url, view_function, name=None):
-        if isinstance(view_function, str):
-            try:
-                function = import_module(str(getModule(view_function)))
-                res = getattr(function, str(getFunction(view_function)))()
-            except:
-                raise ImportError('Module:', view_function, 'not found')
+    def get(self, url, view_class, func_name=None):
+        if isinstance(view_class, Controller):
+            view_function = getattr(view_class(), func_name)
         else:
-            res = view_function
-        app.add_url_rule(url, name, view_func=res, methods=['GET'])
+            view_function = view_class
+        app.add_url_rule(url, func_name, view_func=view_function, methods=['GET'])
         
 
     def post(self, url, view_function, name=None):
